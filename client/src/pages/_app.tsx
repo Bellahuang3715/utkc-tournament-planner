1a312f6baf0721dc0edf6f89d92cbfdc16248a62
Bella Huang <bella.huang@yourenext.ca>
2024-12-21 14:04:44 -0500

    set up skeleton for new navbar and implement the tournaments list page

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-import { EventProvider } from "../context/EventContext";
-import NavBar from "../components/NavBar";
-import { MantineProvider } from "@mantine/core";
+import { ColorSchemeScript, MantineProvider, createTheme } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
+import { BracketSpotlight } from '../components/modals/spotlight';
+// import { EventProvider } from "../context/EventContext";
+// import NavBar from "../components/NavBar";
 import { appWithTranslation } from "next-i18next";
+import Head from 'next/head';
 
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
@@ -10,19 +13,46 @@ import "@mantine/dropzone/styles.css";
 import "@mantine/notifications/styles.css";
 import "@mantine/spotlight/styles.css";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/global.css";
-import "../styles/NavBar.css";
+// import "../styles/NavBar.css";
+
+const theme = createTheme({
+  colors: {
+    dark: [
+      '#C1C2C5',
+      '#A6A7AB',
+      '#909296',
+      '#5c5f66',
+      '#373A40',
+      '#2C2E33',
+      '#25262b',
+      '#1A1B1E',
+      '#141517',
+      '#101113',
+    ],
+  },
+});
 
 const App = ({ Component, pageProps }: any) => (
-  <React.StrictMode>
-    <MantineProvider defaultColorScheme="auto">
-      <EventProvider>
-        <NavBar />
-        <Component {...pageProps} />
-      </EventProvider>
+  <>
+    <Head>
+      <title>Bracket</title>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="shortcut icon" href="/favicon.svg" />
+      {/* <AnalyticsScript /> */}
+
+      <ColorSchemeScript defaultColorScheme="auto" />
+    </Head>
+
+    <MantineProvider defaultColorScheme="auto" theme={theme}>
+      <BracketSpotlight />
+      <Notifications />
+      <Component {...pageProps} />
     </MantineProvider>
-  </React.StrictMode>
+  </>
 );
 
 export default appWithTranslation(App);
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,38 @@
-import React from "react";
-import EventsPage from "../components/EventsPage";
+import { Grid, Title } from "@mantine/core";
+import { GetStaticProps } from "next";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-const HomePage: React.FC = () => {
+import TournamentsCardTable from "../components/card_tables/tournaments";
+import TournamentModal from "../components/modals/tournament_modal";
+import { capitalize } from "../components/utils/util";
+import { checkForAuthError, getTournaments } from "../services/adapter";
+import Layout from "./_layout";
+import classes from "./index.module.css";
+
+export default function HomePage() {
+  const swrTournamentsResponse = getTournaments();
+  checkForAuthError(swrTournamentsResponse);
+  const { t } = useTranslation();
+
   return (
-      <EventsPage />
+    <Layout>
+      <Grid justify="space-between">
+        <Grid.Col span="auto">
+          <Title>{capitalize(t("tournaments_title"))}</Title>
+        </Grid.Col>
+        <Grid.Col span="content" className={classes.fullWithMobile}>
+          <TournamentModal swrTournamentsResponse={swrTournamentsResponse} />
+        </Grid.Col>
+      </Grid>
+      <TournamentsCardTable swrTournamentsResponse={swrTournamentsResponse} />
+    </Layout>
   );
-};
+}
 
-export default HomePage;
+type Props = {};
+export const getStaticProps: GetStaticProps<Props> = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale ?? "en", ["common"])),
+  },
+});
